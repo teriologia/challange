0a12d39a4f243534a670ba542f2fda5949d5a5ee
mehmet Serdar Tekin <serdar.tekin@orginoacademy.com>
2019-06-25 20:54:19 +0300

    unit test created for routes

--- test/index.test.js
+++ test/index.test.js
@@ -5,10 +5,49 @@ const server = require('../app');
 
 chai.use(chaiHttp);
 
-describe('Node start server', () => {
-  it('(GET /) returns main page', (done) =>{
-      chai.request(server)
-      .get('/')
+//will be uncomment when created main page ( localhost:3000/ )
+
+// describe('Node start server', () => {
+//   it('(GET /) returns main page', (done) =>{
+//       chai.request(server)
+//       .get('/')
+//       .end((err, res) => {
+//         res.should.have.status(200);
+//         done();
+//       })
+//   })
+// });
+
+//login page GET simple unit test
+describe('login page', () => {
+  it('(GET /login) returns login page', (done) => {
+    chai.request(server)
+      .get('/login')
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      })
+  })
+});
+
+//login page POST with dummy account simple unit test
+describe('login page', () => {
+  it('(POST /login) returns login page', (done) => {
+    chai.request(server)
+      .post('/login')
+      .send({ email: 'example@example.com', password: 'dummy1234' })
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      })
+  })
+});
+
+//albums page GET simple unit page
+describe('albums page', () => {
+  it('(GET /album) returns album page', (done) => {
+    chai.request(server)
+      .get('/album')
       .end((err, res) => {
         res.should.have.status(200);
         done();
