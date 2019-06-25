70d730860a99397f249a5ed47b08c8e21076af70
mehmet Serdar Tekin <serdar.tekin@orginoacademy.com>
2019-06-26 00:21:47 +0300

    album page styles added

--- src/Js/renderImage.js
+++ src/Js/renderImage.js
@@ -2,21 +2,23 @@
 const renderThumbnail = (data) =>{
     let html = ''; //html to render
     let url = null; //big image url 
-    let limit = 10; //item limit
+    let limit = 8; //item limit
 
     // this loop making long string with data from ajax
     for (let i = 0; i < limit; i++)
     {
-        url = data[i].url
+        url = data[i].url.replace('600', '1920x767')
         html += `
-        <div onClick="renderBigImage('${url}')" data-id=${data[i].id}>
-            <img src=${data[i].thumbnailUrl} />
-        </div>
+        <li>
+            <a href="#!" onClick="renderBigImage('${url}')" data-id=${data[i].id}>
+                <img src=${data[i].thumbnailUrl} />
+            </a>
+        </li>
         `
     }
 
     // rendering html inside images div
-    document.getElementById('images').innerHTML = html
+    document.querySelector('#images ul').innerHTML = html
 }
 
 //Getting url when thumbnail click and rendering image
